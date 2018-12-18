2b52c822020992f4eec607d10f19c44298d8d989
Denis J. González S <dns.gonz@gmail.com>
2018-12-17 19:23:54 -0600

    Add project files.

---
+++ MontyHallParadox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MontyHallParadox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
